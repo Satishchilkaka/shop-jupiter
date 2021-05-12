227d99ca653ff18e8651083af7868cef62f36bcb
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-05-11 22:58:21 -0400

    Add 404.html

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const rootDir = require('./helper/path')
 const indexRoute = require('./routes/index')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(indexRoute.routes)
+app.use('', (req, res, next) => {
+    res.send(path.join(rootDir, 'views', '404.html'))
+})
 app.listen(3000)
