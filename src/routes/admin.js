8156dcb1bda9c552a04b5f58fd6a86636d414efc
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-05-25 23:09:00 -0400

    Convert add-products.html into ejs

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,10 +4,16 @@ const path = require('path')
 const rootDir = require('../helper/path')
 
 router.get('/admin/add-products', (req,res, next) => {
-    res.status(200).sendFile(path.join(rootDir, 'views', 'add-products.html'))
+   // res.status(200).sendFile(path.join(rootDir, 'views', 'add-products.html'))
+
+    res.render('add-products', {
+        path: '/admin/add-products',
+        pageTitle: 'Name'
+    })
 })
 
 router.post('/admin/add-products', (req, res, next) => {
+  //  res.render('add-products',)
 res.redirect('/admin/grocery-products')
 })
 exports.router=router
