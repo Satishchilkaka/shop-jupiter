b51570b4d6c323119fecc5213626fcacd905bf21
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-06-03 22:50:17 -0400

    Update app with ejs chnages

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const pageNotFound = require('./controller/error')
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-// app.set('view engine', 'ejs')
 // app.set('views', 'views')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');  
