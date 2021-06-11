c769e6816efd65f5755d24fafd9934c5e39e2db9
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-06-10 23:38:03 -0400

    Update shope route

---
+++ src/routes/shop.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/grocery-products', (res, req, next)  => {
+    res.status(200)
+    .render('grocery-products', {
+        path: 'grocery-products',
+        pageTitle: 'grocery-products'
+    })
+})
+
+module.exports = router
