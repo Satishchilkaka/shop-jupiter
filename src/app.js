cd450b15c8d38c74339ce7e1829fa31b97923681
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-06-12 22:45:56 -0400

    Update appjs

--- src/app.js
+++ src/app.js
@@ -7,15 +7,16 @@ const adminRoute = require('./routes/admin')
 const pageNotFound = require('./controller/error')
 const shopRoute = require('./routes/shop')
 const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
 // app.set('views', 'views')
+app.set('view engine', 'ejs');  
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyParser.urlencoded({extended: false}))
 
-app.set('view engine', 'ejs');  
+
 app.use(indexRoute.routes)
 app.use(adminRoute.router)
 app.use(shopRoute.router)
